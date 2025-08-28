486d2b4d9f553557293b5a37014d9f923b858286
mnaatjes <michael.naatjes87@gmail.com>
2025-08-27 23:09:40 -0400

    working vue test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,11 +3,16 @@
     <h1>Hello from Vue.js!</h1>
     <p>This is a containerized Vue app served by Apache.</p>
   </div>
+  <my-test></my-test>
 </template>
 
 <script>
+import MyTest from './components/my-test.vue';
 export default {
   name: 'App',
+  components: {
+    MyTest,
+  }
 };
 </script>
 
---
+++ frontend/src/components/my-test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="my-test">
+    <p class="component-text">{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyTest',
+  data() {
+    return {
+      message: 'Hello, Vue Component!',
+    };
+  },
+};
+</script>
+
+<style scoped>
+.my-test {
+  color: #42b983;
+}
+</style>
